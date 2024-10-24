40125d582ce09a54c7d28267cb59eb3d6f507f57
kpayyam1998 <karuppasamy12041998@gmail.com>
2024-10-24 19:20:16 +0530

    api code added

--- agent.py
+++ agent.py
@@ -13,56 +13,63 @@ from langchain.agents import AgentExecutor
 
 load_dotenv()
 
-# Initialize the LLM
-llm = ChatOpenAI(
-    temperature=0,
-    max_tokens=100,
-  
-)
+class LangAgent:
+    def __init__(self):
+        # Initialize the LLM
 
-tool=[SqlQueryWritter,CompanyNameGenerator]
-llm_withtools=llm.bind_tools(tool)
+        self.llm = ChatOpenAI(
+            temperature=0.7,
+            max_tokens=100, # We can give as many as we want 
+        
+        )
 
-def agent():
-    # Create the tools
-
-    MEMORY_KEY = "chat_history"
-    # Create the prompt for the agent
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", "you are a helpful assistant to write queries or summarize queries"),
-            ("user", "{input}"),
-            MessagesPlaceholder(variable_name="agent_scratchpad")
-        ]
-    )
+        self.tool=[SqlQueryWritter,CompanyNameGenerator]
+        self.llm_withtools=self.llm.bind_tools(self.tool)
+        self.MEMORY_KEY = "chat_history"
+        self.chat_history=[]
 
 
-    llm_withtools=llm.bind_tools(tool)
-    agent=(
-        {
-            "input": lambda x: x["input"],
-            "agent_scratchpad": lambda x: format_to_openai_tool_messages(
-                x["intermediate_steps"]
-            ),
-        }
-        | prompt
-        | llm_withtools
-        | OpenAIToolsAgentOutputParser()
-    )
+    def agent(self):
+
+            # Create the prompt for the agent
+            prompt = ChatPromptTemplate.from_messages(
+                [
+                    ("system", "you are a helpful assistant to write queries or summarize queries"),
+                    MessagesPlaceholder(variable_name=self.MEMORY_KEY),
+                    ("user", "{input}"),
+                    MessagesPlaceholder(variable_name="agent_scratchpad"),
 
 
-    agent_executor = AgentExecutor(agent=agent, tools=tool)
+                ]
+            )
 
-    return agent_executor
 
-def generate_response(agent_excutor,user_input):
-    response = agent_excutor.invoke({"input":user_input})
-    return response['output']
+            agent=(
+                {
+                    "input": lambda x: x["input"],
+                    "agent_scratchpad": lambda x: format_to_openai_tool_messages(
+                        x["intermediate_steps"]
+                    ),
+                    "chat_history": lambda x : x['chat_history']
+                }
+                | prompt
+                | self.llm_withtools
+                | OpenAIToolsAgentOutputParser()
+            )
+
+
+            agent_executor = AgentExecutor(agent=agent, tools=self.tool) # verbose=True which will help understand what is the process behind Agents
+
+            return agent_executor
+    def generate_response(self,agent_excutor,user_input):
+            response = agent_excutor.invoke({"input":user_input,"chat_history":self.chat_history})
+            return response['output']
 
 
 
 if __name__=="__main__":
-    agent_excutor=agent()
-    input="I am running pencil company name "
-    res=generate_response(agent_excutor,input)
+    lang_agent = LangAgent()
+    agent_excutor = lang_agent.agent()
+    input = "I am running pencil company name "
+    res = lang_agent.generate_response(agent_excutor,input)
     print(res)
